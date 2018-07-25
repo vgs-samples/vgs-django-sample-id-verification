f11ae326bda4807d214681a4a2b1e8353bb753a0
koponkin <bc9157e16f44f571f01355c7dd7aec26e1df2ae3>
2018-07-25 12:21:43 +0300

    Add data reveal on Detail page. Should be configured inbound reveal on response route. Add checkr client. Add cert proxy validation.

---
+++ app/checker_client.py
@@ -0,0 +1,120 @@
+from datetime import datetime
+from time import sleep
+
+import requests
+
+
+class CheckerServiceException(Exception):
+    def __init__(self, status_code, error):
+        self.status_code = status_code
+        self.error = error
+
+
+class CheckerClientException(Exception):
+    def __init__(self, error):
+        self.error = error
+
+
+class CheckerClient:
+    report_gen_timeout = 10000
+
+    def __init__(self, host, api_key):
+        self.host = host
+        self.api_key = api_key
+
+    def create_candidate(self,
+                         ssn,
+                         dln,
+                         geo_id="486bfc16a8fd6f33519b89af" #San Francisco CA
+                         ):
+        candidate_data = {
+            'first_name': "testName",
+            'no_middle_name': True,
+            'last_name': "TestLastName",
+            'email': "test.email@vgs.io",
+            'phone': "5555555555",
+            'zipcode': "85001",
+            'dob': "1970-01-22",
+            'ssn': ssn,
+            'driver_license_number': dln,
+            'driver_license_state': "CA",
+            "geo_ids": [geo_id]
+        }
+
+        r = requests.post(
+            self.host + '/v1/candidates',
+            json=candidate_data,
+            auth=(self.api_key, ''),
+        )
+
+        if r.status_code != 201:
+            raise CheckerServiceException(r.status_code, r.json()['error'])
+
+        return r.json()['id']
+
+    def create_report(self, candidate_id):
+        candidate_data = {
+            'package': "driver_standard",
+            'candidate_id': candidate_id
+        }
+
+        r = requests.post(
+            self.host + '/v1/reports',
+            json=candidate_data,
+            auth=(self.api_key, '')
+        )
+
+        if r.status_code != 201:
+            raise CheckerServiceException(r.status_code, r.json()['error'])
+
+        return r.json()['id']
+
+    def create_geo(self, name, city, state):
+        candidate_data = {
+            'name': name,
+            'city': city,
+            'state': state
+        }
+
+        r = requests.post(
+            self.host + '/v1/geos',
+            json=candidate_data,
+            auth=(self.api_key, '')
+        )
+
+        if r.status_code != 201:
+            raise CheckerServiceException(r.status_code, r.json()['error'])
+
+        return r.json()['id']
+
+    def retrieve_report(self, report_id):
+        return self.__retrieve_report(report_id, self.report_gen_timeout)
+
+    def __retrieve_report(self, report_id, timeout_sec):
+        if timeout_sec < 0:
+            raise TimeoutError("Requested report were not generated in time")
+
+        start = datetime.now()
+        r = requests.get(
+            self.host + '/v1/reports/'+ report_id,
+            auth=(self.api_key, '')
+        )
+
+        if r.status_code != 200:
+            raise CheckerServiceException(r.status_code, r.json()['error'])
+
+        if r.json()["status"] == "pending":
+            sleep(1)
+            spending_time =  datetime.now() - start
+            spending_time_microsec = spending_time.seconds + 1000 + spending_time.microseconds / 1000
+            remaining_timeout = timeout_sec - spending_time_microsec
+            return self.__retrieve_report(report_id, remaining_timeout)
+
+        if r.json()["status"] in ["clear", "consider"]:
+            return r.json()
+
+        raise CheckerClientException("Could not generate report")
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class PiiData(models.Model):
     social_security_number = models.CharField(max_length=200)
-    national_id = models.CharField(max_length=200)
+    driver_license_number = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.pub_date.__str__() + "  | SSN: " + self.social_security_number \
-               + "; National id: " + self.national_id
+               + "; Driver License Number: " + self.driver_license_number
